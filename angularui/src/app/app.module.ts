15cedd81f72412e3c89e3504c61a19f50ce91c64
busrauludag <busrauludag13@gmail.com>
2018-10-13 19:49:07 +0300

    Author service get function

--- angularui/src/app/app.module.ts
+++ angularui/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AuthorComponent } from './components/author/author.component';
@@ -10,7 +11,8 @@ import { AuthorComponent } from './components/author/author.component';
     AuthorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularui/src/app/services/author.service.ts
+++ angularui/src/app/services/author.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Author } from '../components/author/author';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  path = environment.path;
+
+  getAuthors():Observable<Author[]>{
+    return this.http.get<Author[]>(this.path + '/author')
+  }
 }
