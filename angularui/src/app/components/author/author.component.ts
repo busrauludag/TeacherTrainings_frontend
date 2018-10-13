c358812821e96eb25e64abf3c57618701e01df31
busrauludag <busrauludag13@gmail.com>
2018-10-13 21:07:43 +0300

    listing author datas

--- angularui/src/app/components/author/author.component.ts
+++ angularui/src/app/components/author/author.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthorService } from '../../services/author.service';
+import { Author } from './author';
 
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
-  styleUrls: ['./author.component.css']
+  styleUrls: ['./author.component.css'],
+  providers: [AuthorService]
 })
 export class AuthorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authorService:AuthorService) { }
+  authors:Author[];
 
   ngOnInit() {
+    this.getAuthors();
+  }
+
+  getAuthors(){
+    this.authorService.getAuthors().subscribe(data => {
+      this.authors = data;
+    })
   }
 
 }
--- angularui/src/app/components/author/author.ts
+++ angularui/src/app/components/author/author.ts
@@ -3,5 +3,5 @@ export class Author{
     firstName:string;
     lastName:string;
     email:string;
-    bio:string
+    biography:string
 }
